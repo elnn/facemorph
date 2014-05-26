9d09448ca54a6deb738f170e6309b1323463931f
Daegeun Lee <elnn@elnn.kr>
2014-05-27 01:27:43 +0900

    Migrate from elnn-homepage

---
+++ app.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+import hashlib
+import Image
+import numpy
+import oct2py
+import os.path
+import pymongo
+import scipy.misc
+import StringIO
+import tornado.escape
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+tornado.options.define("port", default=5897, help="run on the given port", type=int)
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        settings = {
+            "cookie_secret": "younhaholic",
+            "static_path": os.path.join(os.path.dirname(__file__), "static"),
+            "template_path": os.path.join(os.path.dirname(__file__), "templates"),
+        }
+        handlers = [
+            (r"/", MainHandler),
+            (r"/view/(.*)", ViewHandler),
+            (r"/list", ListHandler),
+            (r"/upload", UploadHandler),
+            (r"/prepare/(.*)", PrepareHandler),
+            (r"/mix", MixHandler),
+        ]
+        tornado.web.ErrorHandler = ErrorHandler
+        tornado.web.Application.__init__(self, handlers, **settings)
+        self.mongo = pymongo.Connection().elnn
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def db(self):
+        return self.application.mongo
+
+    def get_error_html(self, status_code, **kwargs):
+        return self.render_string("error.html", status_code=status_code)
+
+
+class ErrorHandler(BaseHandler):
+    def __init__(self, application, request, status_code):
+        tornado.web.RequestHandler.__init__(self, application, request)
+        self.set_status(status_code)
+
+    def prepare(self):
+        raise tornado.web.HTTPError(self._status_code)
+
+
+class MainHandler(BaseHandler):
+    @tornado.web.addslash
+    def get(self):
+        self.redirect("/list")
+
+
+class ViewHandler(BaseHandler):
+    def get(self, hash):
+        item = self.db.faces.find_one({"hash": hash})
+        if not item:
+            raise tornado.web.HTTPError(404)
+        else:
+            self.render("view.html", item=item)
+
+
+class ListHandler(BaseHandler):
+    def get(self):
+        items = self.db.faces.find()
+        self.render("list.html", items=items)
+
+
+class UploadHandler(BaseHandler):
+    def get(self):
+        self.render("upload.html")
+
+    def post(self):
+        body = self.request.files["image"][0]["body"]
+        img = Image.open(StringIO.StringIO(body)).resize((400, 400))
+        hash = hashlib.md5(body).hexdigest()[:8]
+        path = os.path.join(self.settings["static_path"], "faces", "%s.jpg" % hash)
+        img.save(path)
+        self.redirect("/prepare/%s" % hash)
+
+
+class PrepareHandler(BaseHandler):
+    def get(self, hash):
+        if self.db.faces.find_one({"hash": hash}):
+            self.redirect("/view/%s" % hash)
+        else:
+            self.render("prepare.html", hash=hash)
+
+    def post(self, hash):
+        if not self.db.faces.find_one({"hash": hash}):
+            points = tornado.escape.json_decode(self.get_argument("points"))
+            self.db.faces.insert({"hash": hash,
+                                  "points": points,
+                                  "parent": None})
+        self.redirect("/view/%s" % hash)
+
+
+class MixHandler(BaseHandler):
+    def post(self):
+        hash1 = self.get_argument("hash1")
+        hash2 = self.get_argument("hash2")
+        if hash1 > hash2:
+            hash1, hash2 = hash2, hash1
+        hash = hashlib.md5(hash1 + hash2).hexdigest()[:8]
+        if not self.db.faces.find_one({"hash": hash}):
+            item1 = self.db.faces.find_one({"hash": hash1})
+            item2 = self.db.faces.find_one({"hash": hash2})
+            if (not item1) or (not item2):
+                raise tornado.web.HTTPError(404)
+            path1 = os.path.join(self.settings["static_path"], "faces", "%s.jpg" % hash1)
+            path2 = os.path.join(self.settings["static_path"], "faces", "%s.jpg" % hash2)
+            img1 = scipy.misc.imread(path1)
+            img2 = scipy.misc.imread(path2)
+            points1 = numpy.array(item1["points"], dtype=float)
+            points2 = numpy.array(item2["points"], dtype=float)
+            ratio = 0.5
+            output = oct2py.octave.call('facemorph', img1, img2, points1, points2, ratio)
+            p = points1 + ratio * (points2 - points1)
+            self.db.faces.insert({"hash": hash,
+                                  "points": p.tolist(),
+                                  "parent": [hash1, hash2]})
+            path = os.path.join(self.settings["static_path"], "faces", "%s.jpg" % hash)
+            scipy.misc.imsave(path, output)
+        self.redirect("/view/%s" % hash)
+
+
+def main():
+    tornado.options.parse_command_line()
+    httpserver = tornado.httpserver.HTTPServer(Application())
+    httpserver.listen(tornado.options.options.port)
+    tornado.ioloop.IOLoop().instance().start()
+
+if __name__ == "__main__":
+    main()
---
+++ facemorph.m
@@ -0,0 +1,50 @@
+function output = facemorph(img1, img2, points1, points2, ratio)
+
+assert(all(size(img1) == size(img2)));
+assert(all(size(points1) == size(points2)));
+img1 = double(img1);
+img2 = double(img2);
+[h, w, nchannels] = size(img1);
+points1 = [points1; 1,1; 1,h; w,1; w,h];
+points2 = [points2; 1,1; 1,h; w,1; w,h];
+
+[X, Y] = meshgrid(1:w, 1:h);
+I = cell(nchannels, 1);
+for channel = 1:nchannels
+    I{channel} = sub2ind([h, w, nchannels], Y(:), X(:), ones(h*w,1)*channel);
+end
+
+p = points1 + ratio*(points2-points1);
+tri = delaunay(p(:,1), p(:,2), 'QJ');
+n = size(tri, 1);
+T = tsearch(p(:,1), p(:,2), tri, X(:), Y(:));
+
+src = cell(n, 1);
+dst = cell(n, 1);
+mix = cell(nchannels, n);
+
+for i = 1:n
+    Xinv = inv([p(tri(i,:),1), p(tri(i,:),2), ones(3,1)]');
+    M1 = [points1(tri(i,:),1), points1(tri(i,:),2), ones(3,1)]' * Xinv;
+    M2 = [points2(tri(i,:),1), points2(tri(i,:),2), ones(3,1)]' * Xinv;
+    f = find(T == i);
+    dummy = [X(f), Y(f), ones(length(f),1)]';
+    src{i} = M1 * dummy;
+    dst{i} = M2 * dummy;
+    for c = 1:nchannels
+        mix{c, i} = I{c}(f);
+    end
+end
+
+src = cat(2, src{:});
+dst = cat(2, dst{:});
+output = ones(h, w, nchannels);
+
+for c = 1:nchannels
+    output(cat(1, mix{c,:})) = ...
+        (1-ratio)*interp2(img1(:,:,c), src(1,:), src(2,:), 'cubic', 1) + ...
+        ratio*interp2(img2(:,:,c), dst(1,:), dst(2,:), 'cubic', 1);
+end
+
+output(find(output < 0)) = 0;
+output = output / max(output(:));
